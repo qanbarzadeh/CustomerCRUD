97ade9e0ea3aadbd56cab0e94a50e9370b3a8d71
qanbarzadeh <ghxalireza@gmail.com>
2023-04-29 17:13:00 +0430

    feature: Add Customer domain entity and tests.

---
+++ Mc2.CrudTest.Domain.Tests/CustomerTest.cs
@@ -0,0 +1,35 @@
+﻿using Mc2.CrudTest.Domain.Entities;
+
+namespace Mc2.CrudTest.Domain.Tests
+{
+
+    public class CustomerTest
+    {
+
+        [Fact]
+        public void CreateCustomer_ShouldSucceed()
+        {
+            // Arrange
+            var firstname = "John";
+            var lastname = "Doe";
+            var dateOfBirth = new DateTime(1985, 1, 1);
+            var phoneNumber = "1234567890";
+            var email = "johndoe@example.com";
+            var bankAccountNumber = "123-456-789";
+
+            // Act
+            var customer = new Customer(firstname, lastname, dateOfBirth, phoneNumber, email, bankAccountNumber);
+
+            // Assert
+            Assert.NotNull(customer);
+            Assert.Equal(firstname, customer.Firstname);
+            Assert.Equal(lastname, customer.Lastname);
+            Assert.Equal(dateOfBirth, customer.DateOfBirth);
+            Assert.Equal(phoneNumber, customer.PhoneNumber);
+            Assert.Equal(email, customer.Email);
+            Assert.Equal(bankAccountNumber, customer.BankAccountNumber);
+        }
+
+
+    }
+}
--- Mc2.CrudTest.Domain/Entities/Customer.cs
+++ Mc2.CrudTest.Domain/Entities/Customer.cs
@@ -5,22 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mc2.CrudTest.Domain
+namespace Mc2.CrudTest.Domain.Entities
 {
     public class Customer
     {
+      
         public int Id { get; set; }
-        
+
         public string Firstname { get; set; }
-        
+
         public string Lastname { get; set; }
-        
+
         public DateTime DateOfBirth { get; set; }
-        
+
         public string PhoneNumber { get; set; }
-        
+
         public string Email { get; set; }
-        
+
         public string BankAccountNumber { get; set; }
+        public Customer(string firstname, string lastname, DateTime dateOfBirth, string phoneNumber, string email, string bankAccountNumber)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            DateOfBirth = dateOfBirth;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            BankAccountNumber = bankAccountNumber;
+        }
     }
 }
